d0ad9220667c19de43059fa30e667afc645ed9d7
Senhui Guo <guo.leciel@gmail.com>
2018-02-09 11:19:51 +0800

    correct path

--- kpd.py
+++ kpd.py
@@ -22,9 +22,9 @@ from model import log
 ROOT_DIR = os.getcwd()
 
 # Directory to save logs and trained model
-MODEL_DIR = os.path.join("/data4T/sguo/logs")
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5.nosync")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
 class LayoutsConfig(Config):
     """Configuration for training on the toy shapes dataset.
